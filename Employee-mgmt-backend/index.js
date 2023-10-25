25db7903e2c3cc1ea4ae4d1288dbfa94cbe8a12f
Nazim8115 <nazim.nn459@gmail.com>
2023-10-25 12:04:58 +0530

    add controller install nodemon

--- Employee-mgmt-backend/index.js
+++ Employee-mgmt-backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
 app.get("/", (req, res) => {
   res.send("this is the home request");
 });
 
-app.listen(8001, () => {
-  console.log("server is running at port ", 8001);
+app.listen(port, () => {
+  console.log(`Server is running at :${port}`);
 });
