bf05d9d429898ecb5e8452b240a760d745f1a58a
Nazim8115 <nazim.nn459@gmail.com>
2023-10-25 13:45:32 +0530

    add schema model

---
+++ Employee-mgmt-backend/models/employee.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+// define employee schema
+
+const employeeSchema = new mongoose.Schema({
+  empId: {
+    type: String,
+    required: true,
+  },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone_number: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+  },
+  job_title: {
+    type: String,
+  },
+  salary: {
+    type: Number,
+  },
+  hire_date: {
+    type: Date,
+  },
+  emp_status: {
+    type: String,
+    required: true,
+  },
+  supervisor: {
+    type: String,
+  },
+  leave_balance: {
+    type: Number,
+  },
+  profile_picture: {
+    String,
+  },
+});
