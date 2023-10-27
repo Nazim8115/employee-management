7d350463182af39b18817f7b7eb928bc1293022f
Nazim8115 <nazim.nn459@gmail.com>
2023-10-27 17:19:41 +0530

    render data inside the table using jquery

--- Employee-mgmt-frontend/script.js
+++ Employee-mgmt-frontend/script.js
@@ -5,9 +5,21 @@ $(document).ready(function () {
     dataType: "json",
     success: function (data) {
       // Process the fetched data here
-      data.forEach((element) => {
-        console.log(element);
+      let tableData = "";
+      data.map((values) => {
+        tableData += `<tr>
+       <td>${values.emp_id}</td>
+       <td>${values.first_name}</td>
+       <td>${values.last_name}</td>
+       <td>${values.email}</td>
+       <td>${values.phone_number}</td>
+       <td>${values.department}</td>
+       <td>${values.job_title}</td>
+       <td>${values.emp_status}</td>
+       <td><img src = "${values.profile_picture}" alt="profile image"/></td>
+       </tr>`;
       });
+      $("#table-body").html(tableData);
     },
     error: function (jqXHR, textStatus, errorThrown) {
       // Handle errors here
